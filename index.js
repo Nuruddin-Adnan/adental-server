7400448894ca200da79c3467badadddb6d687fd3
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-11-09 08:26:48 +0600

    database connect

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+var cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.udttjtr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db('adental').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Adental app listening on port ${port}`)
+})
